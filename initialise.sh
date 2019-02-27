aadc2e2a1630dac727558b51d675594d802503a2
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-02-27 09:04:22 +0200

    changed structure and functionality

--- initialise.sh
+++ initialise.sh
@@ -5,15 +5,14 @@
 
 setup_node1() {
     echo "Starting the configuration of the first node"
-    parity --config node0.toml &
+    parity --config ./data/node0_setup.toml &
     
     echo "Waiting for the node to start"
     sleep 5 # I paused it for five second just to make sure it has enought time to start on any machine 
 
     # creating the first account with the address 0x00bd138abd70e2f00903268f3db08f2d25677c9e
     echo "First account is being created"
-    curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node0", "node0"],"id":0}' -H
-    "Content-Type: application/json" -X POST localhost:8540
+    curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node0", "node0"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8540
 
     # creating the user account with the password 0x004ec07d2329997267ec62b4166639513386f32e
     echo "Second account is being created"
@@ -27,7 +26,7 @@ setup_node1() {
 setup_node2() {
     
     echo "Starting the configuration of the second node"
-    parity --config node1.toml &
+    parity --config ./data/node1_setup.toml &
     
     echo "Waiting for the node to start"
     sleep 5 # I paused it for five second just to make sure it has enought time to start on any machine 
@@ -36,9 +35,13 @@ setup_node2() {
     echo "Second node account is being created"
     curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node1", "node1"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
 
-    # killing the node which is running on port 8540
+    # killing the node which is running on port 8541
     fuser -k 8541/tcp
 }
 
+echo "starting setup"
+
 setup_node1
 setup_node2
+
+echo "setup executed"
